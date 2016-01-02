4af30e85e37f4fc013f7fc95cdc00ef23c6710d3
Rob Garrison <wowmotty@gmail.com>
2016-01-01 22:11:24 -0600

    version bump

--- github-dark-script.user.js
+++ github-dark-script.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         GitHub Dark Script
-// @version      0.8.4
+// @version      0.8.5
 // @description  GitHub Dark in userscript form, with a settings panel
 // @namespace    https://github.com/StylishThemes
 // @include      /https?://((gist|guides|help|raw|status|developer)\.)?github\.com((?!generated_pages\/preview).)*$/
