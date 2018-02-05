119f85e3c34a8aa5f2177a8573781570902274d2
Rob Garrison <wowmotty@gmail.com>
2018-02-05 17:54:18 -0600

    2.4.4

--- github-dark-script.user.js
+++ github-dark-script.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        GitHub Dark Script
-// @version     2.4.3
+// @version     2.4.4
 // @description GitHub Dark in userscript form, with a settings panel
 // @license     MIT
 // @author      StylishThemes
