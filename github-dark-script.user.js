9054a085342df545493cb9f76221b7f3d16165f7
silverwind <me@silverwind.io>
2019-02-28 07:13:42 +0100

    2.5.0

--- github-dark-script.user.js
+++ github-dark-script.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        GitHub Dark Script
-// @version     2.4.15
+// @version     2.5.0
 // @description GitHub Dark in userscript form, with a settings panel
 // @license     MIT
 // @author      StylishThemes
